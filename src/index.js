3c7d3fb52c94206baacfdc7557a818120cf7e622
marcin-bojar <marcin.bojar@interia.pl>
2020-09-06 12:29:25 +0200

    Disconnected Directory component from redux, all app converted to be using apollo-cache, added apollo-cache-persist to keep the persistance of data

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { ApolloProvider } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-boost';
+import { persistCache } from 'apollo-cache-persist';
 
 import { store, persistor } from './redux/store';
 
@@ -20,6 +21,12 @@ const httpLink = createHttpLink({
 });
 
 const cache = new InMemoryCache();
+
+(async () =>
+  await persistCache({
+    cache,
+    storage: window.localStorage,
+  }))();
 
 const client = new ApolloClient({
   link: httpLink,
